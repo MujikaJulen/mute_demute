9c7f3625d8498179850b5a9aaa0c5bf765d02e22
mujikajulenuni1801 <mujikajulenuni@gmail.com>
2026-02-20 18:49:58 +0100

    yeeepa

--- RedNeuronal/model.py
+++ RedNeuronal/model.py
@@ -1,5 +1,5 @@
 import torch
-
+import torch.nn as nn
 
 
 def get_device(force: str = "auto") -> torch.device:
@@ -14,3 +14,24 @@ def get_device(force: str = "auto") -> torch.device:
         return torch.device("cuda")
     # auto
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class CnnJulen(nn.Module):
+    def __init__(self):
+        super(CnnJulen, self).__init__()
+
+        self.conv1 = nn.Conv2d(input_channels=3, out_channels=16, kernel_size=3)
+        self.mp1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=None)
+        self.relu1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(input_channels=3, out_channels=32, kernel_size=3)
+        self.mp2 = nn.MaxPool2d(kernel_size=5, stride=1, padding=None)
+        self.relu2 = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(input_channels=3, out_channels=64, kernel_size=3)
+        self.mp3 = nn.MaxPool2d(kernel_size=5, stride=1, padding=None)
+        self.relu3 = nn.ReLU()
+
+        self.flat = nn.Flatten()
+
+        self.fc1 = nn.Linear(in_features=image_shape, out_features=output_shape)
